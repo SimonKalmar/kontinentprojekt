c4fd0c1ede43e652c11481162c58458bd918441a
Simon Kalmar Riis Mortensen <simonkalmar@gmail.com>
2020-02-17 10:35:32 +0100

    fixed git ignore

---
+++ public/javascripts/continents.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  "Africa": "Africa",
+  "Antarctica": "Antarctica",
+  "Asia": "Asia",
+  "Europe": "Europe",
+  "North America": "North America",
+  "Oceania": "Oceania",
+  "South America": "South America"
+}
